7d90c7bdeedaab0929d92b322b8b2e352c3dd512
Devops3-1 <vinisha.velthuru0216@gmail.com>
2024-12-17 12:21:51 +0530

    Add files via upload

---
+++ arth.java
@@ -0,0 +1,14 @@
+import java.lang.*
+class arth{
+    public static void main(String args[]){
+        int a=10,b=5,c,d,e,f;
+        System.out.println("addition");
+        c=a+b;
+        System.out.println("subtraction");
+        d=a-b;
+        System.out.println("mutiply");
+        e=a*b;
+        System.out.println("division");
+        f=a/b;
+    }
+}
